1f5060e5cc716821e69fa7421d76a336c49e8cc0
TheZHUeatpig <zhuxiaoran2022@163.com>
2024-03-20 14:22:54 +0800

    first commit

---
+++ src/dividedbyzero/Main.java
@@ -0,0 +1,10 @@
+package dividedbyzero;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(3/0);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author zhuxiaoran
+ *
+ */
+module Errorhanding {
+}

64b502ca4c913d3d354ff8a202ce31cf21186122
TheZHUeatpig <zhuxiaoran2022@163.com>
2024-03-20 16:14:14 +0800

    EO2

--- src/dividedbyzero/Main.java
+++ src/dividedbyzero/Main.java
@@ -1,10 +1,16 @@
 package dividedbyzero;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print(3/0);
+	 
+	 /**
+	 * @param args the command line arguments
+	 */
+	 public static void main(String[] args) {
+		 try {
+	 System.out.println(3/0);
+	} catch (ArithmeticException e) {
+		System.out.printf("捕获的运行异常：%s\n", e.getMessage());
+		
 	}
-
+}
 }
